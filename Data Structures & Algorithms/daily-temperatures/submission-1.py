3b06cce691bc5c6fcdc392a7acfb0b25234ab066
Kevin Blin <kblin@outlook.com>
2026-09-22 21:30:00 +0200

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        temps = deque()
+        idx = deque()
+        res = [0]*len(temperatures)
+        for i, t in enumerate(temperatures):
+            if not temps or t <= temps[-1]:
+                temps.append(t)
+                idx.append(i)
+                continue
+            
+            while temps and t > temps[-1]:
+                tl = temps.pop()
+                t_id = idx.pop()
+                res[t_id] = i-t_id
+            temps.append(t)
+            idx.append(i)
+
+        return res
+        
